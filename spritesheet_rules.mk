9cec413b92b215f80a3aba636966b1cbc18d858a
gelatino95 <gelatino95@gmail.com>
2022-08-11 17:22:13 -0400

    Added Ruin Maniac overworld

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -674,3 +674,7 @@ $(FLDEFFGFXDIR)/secret_power_tree.4bpp: %.4bpp: %.png
 
 $(FLDEFFGFXDIR)/record_mix_lights.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 4 -mheight 1
+	
+
+$(OBJEVENTGFXDIR)/people/ruin_maniac.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -mwidth 2 -mheight 4
