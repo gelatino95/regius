6a3b5c11edcea3c3baa284fcbc05d6a3ba219e21
gelatino95 <gelatino95@gmail.com>
2023-01-20 23:10:13 -0500

    Added palm tree script that can summon wild pokemon

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1059,6 +1059,7 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+	.include "data/scripts/palm_tree.inc"
 
 	.include "data/maps/GatewayIsland/scripts.inc"
 
