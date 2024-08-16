b8cc09f2e43b3bd3ba9b8961de89fe8f2e80377f
gelatino <gelatino95@gmail.com>
2024-08-16 11:49:36 -0400

    Updated Dracaena's team

--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -2170,39 +2170,39 @@ static const struct TrainerMon sParty_Lester3[] = {
 static const struct TrainerMon sParty_Dracaena[] = {
     {
     .iv = 255,
-    .lvl = 41,
+    .lvl = 50,
     .species = SPECIES_GYARADOS,
     .moves = {MOVE_ROAR, MOVE_FLAMETHROWER, MOVE_HYPER_BEAM, MOVE_TWISTER}
     },
     {
     .iv = 255,
-    .lvl = 41,
+    .lvl = 50,
     .species = SPECIES_ALGOREST,
     .moves = {MOVE_SUNNY_DAY, MOVE_SOLAR_BEAM, MOVE_SYNTHESIS, MOVE_GROWTH}
     },
     {
     .iv = 255,
-    .lvl = 41,
+    .lvl = 50,
     .species = SPECIES_DUNKETRA,
-    .moves = {MOVE_WATERFALL, MOVE_SUBMISSION, MOVE_SKULL_BASH, MOVE_PROTECT}
+    .moves = {MOVE_WATERFALL, MOVE_SUBMISSION, MOVE_SKULL_BASH, MOVE_CURSE}
     },
     {
     .iv = 255,
-    .lvl = 41,
+    .lvl = 50,
     .species = SPECIES_COCKATRIX,
     .moves = {MOVE_BLIZZARD, MOVE_FIRE_BLAST, MOVE_THUNDER, MOVE_DRAGON_BREATH}
     },
     {
     .iv = 255,
-    .lvl = 41,
-    .species = SPECIES_FLYGON,
-    .moves = {MOVE_DRAGON_CLAW, MOVE_DIG, MOVE_FLAMETHROWER, MOVE_ROCK_TOMB}
+    .lvl = 50,
+    .species = SPECIES_KINGDRA,
+    .moves = {MOVE_RAIN_DANCE, MOVE_HYDRO_PUMP, MOVE_ICE_BEAM, MOVE_PROTECT}
     },
     {
     .iv = 255,
-    .lvl = 43,
+    .lvl = 52,
     .species = SPECIES_TERRISLA,
-    .heldItem = ITEM_SITRUS_BERRY,
+    .heldItem = ITEM_MYSTIC_WATER,
     .moves = {MOVE_WATER_SPOUT, MOVE_EARTHQUAKE, MOVE_DRAGON_BREATH, MOVE_SURF}
     }
 };
